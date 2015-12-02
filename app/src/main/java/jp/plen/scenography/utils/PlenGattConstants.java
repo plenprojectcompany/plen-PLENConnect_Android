5344309712c19698e3f61b3333052539c52d7bb5
kzm4269 <4269kzm@gmail.com>
2015-12-02 17:41:41 +0900

    fix PLEN scan filters

--- app/src/main/java/jp/plen/scenography/utils/PlenGattConstants.java
+++ app/src/main/java/jp/plen/scenography/utils/PlenGattConstants.java
@@ -14,13 +14,11 @@ public final class PlenGattConstants {
     private PlenGattConstants() {
     }
 
-    public static final String GAP_DEVICE_NAME = "PLEND";
     public static final UUID PLEN_CONTROL_SERVICE_UUID = UUID.fromString("E1F40469-CFE1-43C1-838D-DDBC9DAFDDE6");
     public static final UUID RX_DATA_UUID = UUID.fromString("2ED17A59-FC21-488E-9204-503EB78158D7");
     public static final UUID TX_DATA_UUID = UUID.fromString("F90E9CFE-7E05-44A5-9D75-F13644D6F645");
     public static final List<ScanFilter> PLEN_FILTERS = Collections.singletonList(
             new ScanFilter.Builder()
-                    .setDeviceName(GAP_DEVICE_NAME)
                     .setServiceUuid(new ParcelUuid(PLEN_CONTROL_SERVICE_UUID))
                     .build());
 }
